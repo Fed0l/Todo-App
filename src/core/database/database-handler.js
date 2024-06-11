b3c93770347c519df95d1557b18add0ddae35169
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-06-11 19:32:07 +0330

    add database-handler to project

---
+++ src/core/database/database-handler.js
@@ -0,0 +1,20 @@
+import pg from "pg";
+import { postgressEssentails } from "../secret/databaseSecrets.js";
+
+const { Client } = pg;
+
+export const query = async (sql, params) => {
+  let client;
+  try {
+    client = new Client(postgressEssentails);
+    await client.connect();
+    const res = await client.query(sql, params);
+    return res.rows;
+  } catch (err) {
+    console.error(err);
+    return null;
+  } finally {
+    await client.end();
+    console.log("connection closed");
+  }
+};
