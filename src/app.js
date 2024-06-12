e255ebbdb0604a8ef2e0096608e09986b97eba08
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-06-12 22:03:19 +0330

    add app and routes to project

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { router } from "./modules/todo/routes.js";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   res.json({
     message: "Hello World!",
@@ -12,7 +14,6 @@ app.get("/test", (req, res) => {
 });
 
 app.use(router);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
