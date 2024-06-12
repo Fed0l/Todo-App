8dc1403998b48f93da360e138afebe4c8e3cc389
Fedros Mohseni <mohseni.fedros@gmail.com>
2024-06-12 22:57:27 +0330

    finalize app

--- src/app.js
+++ src/app.js
@@ -18,31 +18,3 @@ app.use(router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// getall()
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
-
-// getTodoByID('3')
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
-
-// createNewItem("aqwsda", "asdqwqqsadasda", "True")
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
-
-// updateTodoById('1','ff','mm','false')
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
-
-// deleteTodoById('3')
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
-
-// deleteAllTodos()
-// .then ((data) => console.log(data))
-// .catch((err) => console.error(err));
-
-// updateAllTodosStatus()
-// .then ((data) => console.log(data))
-// .catch((err) => console.error(err));
